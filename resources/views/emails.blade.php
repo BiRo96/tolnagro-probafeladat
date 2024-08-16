93bd5778c4ddd827a59effb5dda12500ef041952
Bihary Roland <rolandbihary@gmail.com>
2024-08-16 08:09:12 +0200

    tailwindcss

--- resources/views/emails.blade.php
+++ resources/views/emails.blade.php
@@ -5,41 +5,22 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     <title>E-mail statisztikák</title>
-    <style>
-        table {
-            border: 1px solid #e0e0e0
-        }
-        th, td {
-            padding: 15px
-        }
-        tr th{
-            background-color: #e0e0e0
-        }
-        tr td:nth-child(even) {
-            text-align: right
-        }
-        tr td:nth-child(odd) {
-            text-align: center
-        }
-        tr:nth-child(even) {
-            background-color: #f2f2f2
-        }
-    </style>
+    @vite('resources/css/app.css')
 </head>
 <body>
-    <h1>E-mail statisztikák</h1>
-    <table>
+    <h1 class="text-3xl m-10 font-bold">E-mail statisztikák</h1>
+    <table class="m-10 border border-gray-300">
         <thead>
-        <tr>
-            <th>E-mail</th>
-            <th>Kiküldések száma</th>
+        <tr class="bg-gray-300">
+            <th class="p-4">E-mail</th>
+            <th class="p-4">Kiküldések száma</th>
         </tr>
         </thead>
         <tbody>
             @foreach ($sent_emails as $item)
-                <tr>
-                    <td>{{$item->emailTemplate->subject}}</td>
-                    <td>{{$item['count']}}</td>
+                <tr class="even:bg-gray-100">
+                    <td class="p-4 text-center">{{$item->emailTemplate->subject}}</td>
+                    <td class="p-4 text-right">{{$item['count']}}</td>
                 </tr>
             @endforeach
         </tbody>
