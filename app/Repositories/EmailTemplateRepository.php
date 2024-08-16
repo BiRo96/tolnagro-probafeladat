c0468978722610380d96e7de92d8b92f9b2c2cb7
Bihary Roland <rolandbihary@gmail.com>
2024-08-16 17:31:22 +0200

    repository kommentelés

--- app/Repositories/EmailTemplateRepository.php
+++ app/Repositories/EmailTemplateRepository.php
@@ -20,26 +20,45 @@ interface EmailTemplateRepositoryInterface
 
 class EmailTemplateRepository implements EmailTemplateRepositoryInterface
 {
+    /**
+     * Get all email templates
+     */
     public function getAll() : Collection
     {
         return EmailTemplate::all();
     }
 
+    /**
+     * Get all email templates with the records of sent emails
+     */
     public function getAllWithSentEmails() : Collection
     {
         return EmailTemplate::with('sentEmails')->get();
     }
 
+    /**
+     * Create email template
+     * 
+     * @param array $data The data to create the email template
+     */
     public function create(array $data) : void
     {
         EmailTemplate::create($data);
     }
 
+    /**
+     * Delete email template
+     * 
+     * @param string $id The id of the email template we want to delete
+     */
     public function delete(string $id) : void
     {
         EmailTemplate::destroy($id);
     }
 
+    /**
+     * Truncate email templates
+     */
     public function truncate() : void
     {
         EmailTemplate::truncate();
--- app/Repositories/SentEmailRepository.php
+++ app/Repositories/SentEmailRepository.php
@@ -14,11 +14,19 @@ interface SentEmailRepositoryInterface
 
 class SentEmailRepository implements SentEmailRepositoryInterface
 {
+    /**
+     * Create a record of a sent email (not actually sending the email)
+     * 
+     * @param array $data The data to create the sent email record
+     */
     public function create(array $data) : void
     {
         SentEmail::create($data);
     }
 
+    /**
+     * Truncate sent emails
+     */
     public function truncate() : void
     {
         SentEmail::truncate();
