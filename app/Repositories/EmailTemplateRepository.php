bf09e7115afd95e24f428095318c1dd195f0e5ae
Bihary Roland <rolandbihary@gmail.com>
2024-08-16 18:54:42 +0200

    repository dependency-injection

--- app/Repositories/EmailTemplateRepository.php
+++ app/Repositories/EmailTemplateRepository.php
@@ -9,31 +9,35 @@ use Illuminate\Database\Eloquent\Collection;
 class EmailTemplateRepository implements EmailTemplateRepositoryInterface
 {
 
+    public function __construct(private EmailTemplate $emailTemplate)
+    {
+    }
+
     public function getAll() : Collection
     {
-        return EmailTemplate::all();
+        return $this->emailTemplate->all();
     }
 
     
     public function getAllWithSentEmails() : Collection
     {
-        return EmailTemplate::with('sentEmails')->get();
+        return $this->emailTemplate->with('sentEmails')->get();
     }
 
 
     public function create(array $data) : void
     {
-        EmailTemplate::create($data);
+        $this->emailTemplate->create($data);
     }
 
 
     public function delete(string $id) : void
     {
-        EmailTemplate::destroy($id);
+        $this->emailTemplate->destroy($id);
     }
 
     public function truncate() : void
     {
-        EmailTemplate::truncate();
+        $this->emailTemplate->truncate();
     }
 }
--- app/Repositories/SentEmailRepository.php
+++ app/Repositories/SentEmailRepository.php
@@ -7,13 +7,18 @@ use App\Repositories\Interfaces\SentEmailRepositoryInterface;
 
 class SentEmailRepository implements SentEmailRepositoryInterface
 {
+
+    public function __construct(private SentEmail $sentEmail)
+    {
+    }
+
     public function create(array $data) : void
     {
-        SentEmail::create($data);
+        $this->sentEmail->create($data);
     }
 
     public function truncate() : void
     {
-        SentEmail::truncate();
+        $this->sentEmail->truncate();
     }
 }
